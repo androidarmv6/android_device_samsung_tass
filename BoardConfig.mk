26c1c9f0cd8f4dcbd401346a8ed988dc407c3564
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2014-01-02 16:50:38 +0530

    tass: Camera Shutter Hack
    Change-Id: I0afd2fbf42cadede822b68303facdca57dc62b50

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,3 +31,7 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
 ## Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/tass/bluetooth
 
+
+## Camera
+BOARD_CAMERA_SHUTTER_HACK := true
+CAMERA_FC_HACK := true
